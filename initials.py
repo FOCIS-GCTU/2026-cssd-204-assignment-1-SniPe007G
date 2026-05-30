90ecb662df535c0606226e573aa3e6bf6cf5674c
SniPe007G <2425404102@live.gctu.edu.gh>
2026-05-30 20:58:04 +0000

    Implement stylized initials printing in initials.py
    Added a program to print initials 'ERO' in stylized large letters.

--- initials.py
+++ initials.py
@@ -1,7 +1,38 @@
 
 def main():
   pass #code goes here
+  # File: initials.py
+# Description: Print out my initials ERO in stylized large letters.
+# Assignment Number: 2
+#
+# Name: Eshun Ruben Ofori
+# STUDENT ID: 2425404102
+# Email: eshunrubenofori223@gmail.com / 2425404102@live.gctu.edu.gh
+# Grader: Augustus
+# On my honor, Eshun Ruben Ofori, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 
-
+def main():
+    # Blank line before small initials
+    print()
+    # Print small initials line with three periods
+    print("...ERO")
+    # Print blank line before large initials
+    print()
+    # Large initials
+    # 12 characters wide,10 lines high
+    print("...EEEEEEEEEEEE........RRRRRRRRRR.............OOOOOO........")
+    print("...EEE.................RR.......RR..........OO......OO......")
+    print("...EEE.................RR.......RR.........OO........OO.....")
+    print("...EEE.................RR.......RR.........OO........OO.....")
+    print("...EEEEEEEEEEEE........RRRRRRRR............OO........OO.....")
+    print("...EEEEEEEEEEEE........RRRRRR..............OO........OO.....")
+    print("...EEE.................RR....RR............OO........OO.....")
+    print("...EEE.................RR.....RR...........OO........OO.....")
+    print("...EEE............**...RR......RR.....**....OO......OO....**")
+    print("...EEEEEEEEEEEE...**...RR.......RR....**......OOOOOO......**")
+    # Print blank line at the end
+    print()
+    #Call the main function to run the program
 main()
